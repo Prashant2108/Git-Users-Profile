0327872911db8d7140db734356f6285a8cae7f96
Prashant Agnihotri <49424915+Prashant2108@users.noreply.github.com>
2020-04-05 10:10:54 -0700

    Updates

---
+++ precache-manifest.4c8b66a0ae41ba0562178253fb559c1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7991102e07488883008a3553b76341b6",
+    "url": "/Git-Users-Profile/index.html"
+  },
+  {
+    "revision": "b27fcc58d6248124dee8",
+    "url": "/Git-Users-Profile/static/css/main.05242c3a.chunk.css"
+  },
+  {
+    "revision": "c036f5b2f285ea6d2737",
+    "url": "/Git-Users-Profile/static/js/2.8c5d8d71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Git-Users-Profile/static/js/2.8c5d8d71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b27fcc58d6248124dee8",
+    "url": "/Git-Users-Profile/static/js/main.a431873c.chunk.js"
+  },
+  {
+    "revision": "1be9a89dad7630828f0b",
+    "url": "/Git-Users-Profile/static/js/runtime-main.3a3f51aa.js"
+  }
+]);
